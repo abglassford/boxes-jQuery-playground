388e11d13012dddde270cd0ccbb0c72fe241cda5
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-20 19:40:35 -0600

    work on problems

--- boxes.js
+++ boxes.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+ $(document).ready(function(){
+  alert("ready for DOM manipulation!");
+  // $('#secretBox').css('background-color', 'white');
+  // $('<h1>Secret Box!</h1>').appendTo('#secretBox')
+  // $('#row1').children().attr('class', 'boxType3')
+  // $('#row4').children().last().hide()
+  // $('.boxType1').css('background-color', 'white')
+  //$('#row2').children().last().prev().prevAll().css('background-color', 'transparent')
+  // $('#container').children().children().not('#secretBox').css('width', '2px')//
+  // $('#container').click(function (){
+  //   console.log('x: ' + event.pageX + ', y: ' + event.pageY)
+  // })
+  // $('<a href="http://www.galvanize.com/">galvanize</a>').appendTo('.boxType1')
+  // $('a').on('click', function() {
+  //   alert('YOU CAN NEVER LEAVE!');
+  // });
+  // $('.box').on('click', function(){
+  //     $(this).css('background-image', 'url(pup.jpg)')
+  //     $(this).on('click', function(){
+  //       $(this).css('background-image', 'none')
+  //     })
+  // })
+  $('#container').on('click',function(){
+    $(this).css('background-color', 'black')
+  })
+
+});
