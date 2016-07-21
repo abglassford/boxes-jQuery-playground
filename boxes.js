e624438789643b3243642c13a9c920b30987a8e9
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-20 20:38:31 -0600

    progress

--- boxes.js
+++ boxes.js
@@ -5,14 +5,16 @@
   // $('#row1').children().attr('class', 'boxType3')
   // $('#row4').children().last().hide()
   // $('.boxType1').css('background-color', 'white')
-  //$('#row2').children().last().prev().prevAll().css('background-color', 'transparent')
+  // $('#row2').children().last().prev().prevAll().removeClass()
   // $('#container').children().children().not('#secretBox').css('width', '2px')//
   // $('#container').click(function (){
   //   console.log('x: ' + event.pageX + ', y: ' + event.pageY)
   // })
   // $('<a href="http://www.galvanize.com/">galvanize</a>').appendTo('.boxType1')
+  //
   // $('a').on('click', function() {
   //   alert('YOU CAN NEVER LEAVE!');
+  //   return false
   // });
   // $('.box').on('click', function(){
   //     $(this).css('background-image', 'url(pup.jpg)')
@@ -22,6 +24,7 @@
   // })
   $('#container').on('click',function(){
     $(this).css('background-color', 'black')
+    $('.box').css('background-color', 'white')
   })
 
 });
